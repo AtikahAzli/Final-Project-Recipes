85f1d114c2dcdaba9ee8f242177e9f0693f66651
Nurul Atikah <nurulatikahazli@gmail.com>
2022-11-23 15:38:27 +0800

    Add new shuffle button
    - Add on shuffle button to display random recipe
    - Changing the style

--- script.js
+++ script.js
@@ -1,59 +1,112 @@
-//Initial References
-let result = document.getElementById("result");
-let searchBtn = document.getElementById("search-btn");
-let url = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
+let output = document.getElementById("output");
+let btnSrch = document.getElementById("search-btn");
+let btnRan = document.getElementById("random");
+let urlAPI = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
+let ranAPI = "https://www.themealdb.com/api/json/v1/1/random.php";
 
+let meal="" ; let count=0; let ingredients=""; let ingre=""; let measure="";
+let ingredientCon = ""; let parent=""; let child=""; let recipe=""; let hideRecipe=""; let showRecipe="";
 
-searchBtn.addEventListener("click", () => {
-  let userInp = document.getElementById("user-inp").value;
-  if (userInp.length == 0) {
-    result.innerHTML = `<h3>Input Field Cannot Be Empty</h3>`;
+btnRan.addEventListener("click", () => {
+  fetch(ranAPI)
+    .then((response) => response.json())
+    .then((data) => {
+      meal = data.meals[0];
+      count = 1;
+      let ingredients = [];
+      for (let i in meal) {
+        ingre = "";
+        measure = "";
+        if (i.startsWith("strIngredient") && meal[i]) {
+          ingre = meal[i];
+          measure = meal[`strMeasure` + count];
+          count += 1;
+          ingredients.push(`${measure} ${ingre}`);
+        }
+      }
+
+      output.innerHTML = `
+  <img src=${meal.strMealThumb}>
+  <div class="details">
+      <h2>${meal.strMeal}</h2>
+      <h4>${meal.strArea}</h4>
+  </div>
+  <div id="ingredient-con"></div>
+  <div id="recipe">
+      <button id="hide-recipe">X</button>
+      <pre id="instructions">${meal.strInstructions}</pre>
+  </div>
+  <button id="show-recipe">View Recipe</button>
+  `;
+      ingredientCon = document.getElementById("ingredient-con");
+      parent = document.createElement("ul");
+      recipe = document.getElementById("recipe");
+      hideRecipe = document.getElementById("hide-recipe");
+      showRecipe = document.getElementById("show-recipe");
+
+      ingredients.forEach((i) => {
+        child = document.createElement("li");
+        child.innerText = i;
+        parent.appendChild(child);
+        ingredientCon.appendChild(parent);
+      });
+
+      hideRecipe.addEventListener("click", () => {
+        recipe.style.display = "none";
+      });
+      showRecipe.addEventListener("click", () => {
+        recipe.style.display = "block";
+      });
+    })
+    .catch(() => {
+      output.innerHTML = `<h3>No match found</h3>`;
+    });
+});
+
+btnSrch.addEventListener("click", () => {
+  let userInput = document.getElementById("user-inp").value;
+  if (userInput.length == 0) {
+    output.innerHTML = `<h3>Please input any meal</h3>`;
   } else {
-    fetch(url + userInp)
+    fetch(urlAPI + userInput)
       .then((response) => response.json())
       .then((data) => {
-        let myMeal = data.meals[0];
-        console.log(myMeal);
-        console.log(myMeal.strMealThumb);
-        console.log(myMeal.strMeal);
-        console.log(myMeal.strArea);
-        console.log(myMeal.strInstructions);
-        let count = 1;
-        let ingredients = [];
-        for (let i in myMeal) {
-          let ingredient = "";
-          let measure = "";
-          if (i.startsWith("strIngredient") && myMeal[i]) {
-            ingredient = myMeal[i];
-            measure = myMeal[`strMeasure` + count];
+        meal = data.meals[0];
+        count = 1;
+        ingredients = [];
+        for (let j in meal) {
+          ingre = "";
+          measure = "";
+          if (j.startsWith("strIngredient") && meal[j]) {
+            ingre = meal[j];
+            measure = meal[`strMeasure` + count];
             count += 1;
-            ingredients.push(`${measure} ${ingredient}`);
+            ingredients.push(`${measure} ${ingre}`);
           }
         }
-        console.log(ingredients);
 
-        result.innerHTML = `
-    <img src=${myMeal.strMealThumb}>
+        output.innerHTML = `
+    <img src=${meal.strMealThumb}>
     <div class="details">
-        <h2>${myMeal.strMeal}</h2>
-        <h4>${myMeal.strArea}</h4>
+        <h2>${meal.strMeal}</h2>
+        <h4>${meal.strArea}</h4>
     </div>
     <div id="ingredient-con"></div>
     <div id="recipe">
         <button id="hide-recipe">X</button>
-        <pre id="instructions">${myMeal.strInstructions}</pre>
+        <pre id="instructions">${meal.strInstructions}</pre>
     </div>
     <button id="show-recipe">View Recipe</button>
     `;
-        let ingredientCon = document.getElementById("ingredient-con");
-        let parent = document.createElement("ul");
-        let recipe = document.getElementById("recipe");
-        let hideRecipe = document.getElementById("hide-recipe");
-        let showRecipe = document.getElementById("show-recipe");
+        ingredientCon = document.getElementById("ingredient-con");
+        parent = document.createElement("ul");
+        recipe = document.getElementById("recipe");
+        hideRecipe = document.getElementById("hide-recipe");
+        showRecipe = document.getElementById("show-recipe");
 
-        ingredients.forEach((i) => {
-          let child = document.createElement("li");
-          child.innerText = i;
+        ingredients.forEach((j) => {
+          child = document.createElement("li");
+          child.innerText = j;
           parent.appendChild(child);
           ingredientCon.appendChild(parent);
         });
@@ -66,7 +119,7 @@ searchBtn.addEventListener("click", () => {
         });
       })
       .catch(() => {
-        result.innerHTML = `<h3>Invalid Input</h3>`;
+        output.innerHTML = `<h3>No match found</h3>`;
       });
   }
 });
